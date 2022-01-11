789e4b5e10580be555df0eadd7b46e390a0edb12
annie127 <haw21002@byui.edu>
2022-01-11 22:40:14 +0800

    created my portfolio

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const linksContent = [
+    {
+      label: "Week 1 notes",
+      url: "week01/index.html"
+    },
+    {
+        label: "Week 2 notes",
+        url: "week02/index.html"
+      }
+  ]
+
+      for (let i = 0; i < linksContent.length; i++ ) {
+
+        let myList = document.createElement('li');
+        let myLinks = document.createElement('a');
+
+        myLinks.textContent = linksContent[i].label;
+        myLinks.setAttribute("href", linksContent[i].url);
+    
+        myList.appendChild(myLinks);
+        
+        document.querySelector('.ToC').appendChild(myList);
+
+      }
