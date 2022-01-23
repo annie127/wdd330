549028cf4c1353c5f5b93812102920cf0706a260
annie127 <haw21002@byui.edu>
2022-01-23 09:38:13 +0800

    week 3 reading

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 const linksContent = [
     {
-      label: "Week 1",
+      label: "Week 1: Basics of Website Development - Local Storage",
       url: "week01/index.html"
     },
     {
-        label: "Week 2",
+        label: "Week 2: Programming Basics - Arrays - Logic - Loops - Functions",
         url: "week02/index.html"
-      }
+      },
+      {
+      label: "Week 3: Objects - Document Object Model - Events",
+      url: "week03/index.html"
+    }
+
   ]
 
       for (let i = 0; i < linksContent.length; i++ ) {
